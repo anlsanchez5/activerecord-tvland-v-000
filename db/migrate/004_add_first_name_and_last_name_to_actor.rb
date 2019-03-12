84b483a8d013be9cf95662540023381e58663da3
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-12 21:13:04 +0000

    Automatically backed up by Learn

--- db/migrate/004_add_first_name_and_last_name_to_actor.rb
+++ db/migrate/004_add_first_name_and_last_name_to_actor.rb
@@ -1,4 +1,4 @@
-class AddFirstNameAndLastNameToActor < ActiveRecord::Migration[4.2]
+class CreateActors < ActiveRecord::Migration[4.2]
   def change
     add_column :actors, :first_name, :string
     add_column :actors, :last_name, :string
