51e3cf9fc4d8c6c37c5128e7b441a497cc97e257
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-12 21:59:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(name:nil)
-    self.network = Network.create(name:)
+    self.network = Network.create(name:nil)
 
   end
 end
