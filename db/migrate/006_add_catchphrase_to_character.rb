2bb9c74e968749c3161db22e5e31e586325b359b
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-12 21:27:26 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,4 +1,4 @@
-class AddCatcphraseToCharacter < ActiveRecord::Migration[4.2]
+class AddCatcPhraseToCharacter < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :catchphrase, :string
   end
